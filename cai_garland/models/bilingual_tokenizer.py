4be8d4bcbed1fffbf9f11bf4f46d127153b849b4
Eugene Eisenstein <eeisenst@gmail.com>
2022-06-18 13:23:21 -0400

    Helper bilingual tokenizer class
    Makes one tokenizer that switches between two monolingual tokenizers

---
+++ cai_garland/models/bilingual_tokenizer.py
@@ -0,0 +1,48 @@
+from contextlib import contextmanager
+
+from transformers import PreTrainedTokenizerBase
+
+
+class BilingualTokenizer(PreTrainedTokenizerBase):
+    def __init__(self, source_tokenizer, target_tokenizer, *args, **kwargs):
+        self.source_tokenizer = source_tokenizer
+        self.target_tokenizer = target_tokenizer
+        self._tokenizer = self.source_tokenizer
+        super().__init__(
+            bos_token=self.source_tokenizer.bos_token,
+            eos_token=self.source_tokenizer.eos_token,
+            unk_token=self.source_tokenizer.unk_token,
+            sep_token=self.source_tokenizer.sep_token,
+            pad_token=self.source_tokenizer.pad_token,
+            cls_token=self.source_tokenizer.cls_token,
+            mask_token=self.source_tokenizer.mask_token,
+            *args,
+            **kwargs)
+
+    def _tokenize(self, text, **kwargs):
+        return self._tokenizer._tokenize(text, **kwargs)
+
+    def tokenize(self, text, **kwargs):
+        return self._tokenizer.tokenize(text, **kwargs)
+
+    def convert_tokens_to_ids(self, tokens):
+        return self._tokenizer.convert_tokens_to_ids(tokens)
+
+    def convert_ids_to_tokens(self, ids, skip_special_tokens = False):
+        return self._tokenizer.convert_ids_to_tokens(ids, skip_special_tokens=skip_special_tokens)
+
+    def _convert_token_to_id(self, token):
+        return self._tokenizer._convert_token_to_id(token)
+
+    def _convert_id_to_token(self, index):
+        return self._tokenizer._convert_id_to_token(index)
+
+    def _batch_encode_plus(self, *args, **kwargs):
+        return self._tokenizer._batch_encode_plus(*args, **kwargs)
+
+    @contextmanager
+    def as_target_tokenizer(self):
+        """Switches to the target tokenizer within the context returned by the manager."""
+        self._tokenizer = self.target_tokenizer
+        yield
+        self._tokenizer = self.source_tokenizer
