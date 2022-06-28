98ebb23de642a8acd52cda5b029f3bc228396174
Eugene Eisenstein <eeisenst@gmail.com>
2022-06-28 17:39:03 -0400

    Add basic batch translation to CLI

--- cai_garland/cli/translate.py
+++ cai_garland/cli/translate.py
@@ -3,8 +3,11 @@ import sys
 import logging
 import hydra
 from hydra.utils import instantiate
+import glob
 
-from ..utils.translator import Translator
+from tqdm.auto import tqdm
+
+from ..utils.translator import Translator, TokenizationTooLongException
 
 
 def interactive(translator, cfg):
@@ -18,7 +21,39 @@ def interactive(translator, cfg):
 
 
 def batch(translator, cfg):
-    raise NotImplementedError("Batch processing not yet implemented")
+    if cfg.mode.input_glob is None:
+        raise ValueError("Specify an input file (or glob) in the mode.input_glob setting. You can do this from the "
+                         "command line.")
+    os.makedirs(cfg.mode.output_dir, exist_ok=True)
+    in_fns = glob.glob(cfg.mode.input_glob)
+    for in_fn in tqdm(in_fns, desc="Files"):
+        with open(in_fn, encoding=cfg.mode.encoding) as in_f:
+            bo_text = in_f.read()
+
+        hard_segments = instantiate(cfg.mode.hard_segmentation, bo_text, **dict(cfg.mode.hard_segmenter_kwargs))
+
+        out_fn = os.path.join(
+            cfg.mode.output_dir, os.path.splitext(os.path.basename(in_fn))[0] + '.' + cfg.mode.output_extension)
+        with open(out_fn, mode='w') as out_f:
+            for segment in tqdm(hard_segments, desc="Segments", leave=False):
+                for preproc_func in cfg.mode.preprocessing:
+                    segment = instantiate(preproc_func, segment)
+
+                try:
+                    tgt_segment = translator.translate(segment)
+                except TokenizationTooLongException as err:
+                    if not cfg.mode.skip_long_inputs:
+                        raise err
+                    else:
+                        tgt_segment = "SEGMENT TOKENIZATION TOO LONG FOR ENCODER MODEL"
+
+                for postproc_func in cfg.mode.postprocessing:
+                    tgt_segment = instantiate(postproc_func, tgt_segment)
+
+                if cfg.mode.output_parallel_translation:
+                    out_f.write(segment + '\n')
+                out_f.write(tgt_segment + '\n')
+                out_f.write('\n')
 
 
 @hydra.main(version_base="1.2", config_path="./translate.config", config_name="config")
@@ -32,6 +67,8 @@ def main(cfg):
 
     translator = Translator(os.path.join(cfg.model.model_ckpt, cfg.model.model_size))
     translator.num_beams = cfg.generation.num_beams
+    if cfg.cuda:
+        translator.cuda()
 
     instantiate(cfg.mode.process_func, translator, cfg)
 
