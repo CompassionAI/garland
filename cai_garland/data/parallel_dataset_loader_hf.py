952fff53adb5e8d99e60c37bb58ef0836fc267c5
eisene <eeisenst@gmail.com>
2022-08-19 13:44:46 -0400

    Refactor parallel dataset loader

--- cai_garland/data/parallel_dataset_loader_hf.py
+++ cai_garland/data/parallel_dataset_loader_hf.py
@@ -48,53 +48,53 @@ class ParallelSentences84000(datasets.GeneratorBasedBuilder):
     BUILDER_CONFIGS = [
         ParallelSentences84000Config(
             name="no_registers",
-            version=datasets.Version("0.1.1", ""),
+            version=datasets.Version("0.2.0", ""),
             description="Dataset for a Tibetan encoder with no registers",
         ),
         ParallelSentences84000Config(
             name="no_registers_no_splits",
-            version=datasets.Version("0.1.1", ""),
+            version=datasets.Version("0.2.0", ""),
             description="Dataset for a Tibetan encoder with no registers and all splits concatenated",
         ),
         ParallelSentences84000Config(
             name="raw",
-            version=datasets.Version("0.1.1", ""),
+            version=datasets.Version("0.2.0", ""),
             description="Dataset for a Tibetan encoder from the raw parallel sentences",
         ),
         ParallelSentences84000Config(
             name="raw_no_splits",
-            version=datasets.Version("0.1.1", ""),
+            version=datasets.Version("0.2.0", ""),
             description="Dataset for a Tibetan encoder from the raw parallel sentences and all splits concatenated",
         ),
         ParallelSentences84000Config(
             name="raw_no_dict",
-            version=datasets.Version("0.1.1", ""),
+            version=datasets.Version("0.2.0", ""),
             description="Dataset for a Tibetan encoder from the raw parallel sentences and no dictionary",
         ),
         ParallelSentences84000Config(
             name="raw_no_dict_no_splits",
-            version=datasets.Version("0.1.2", ""),
+            version=datasets.Version("0.2.0", ""),
             description="Dataset for a Tibetan encoder from the raw parallel sentences, no dictionary, and all splits "
                         "concatenated",
         ),
         ParallelSentences84000Config(
             name="registers_3",
-            version=datasets.Version("0.1.2", ""),
+            version=datasets.Version("0.2.0", ""),
             description="Dataset for a Tibetan encoder with 3 registers",
         ),
         ParallelSentences84000Config(
             name="registers_3_only",
-            version=datasets.Version("0.1.2", ""),
+            version=datasets.Version("0.2.0", ""),
             description="Dataset for a Tibetan encoder with 3 registers and no augmentation",
         ),
         ParallelSentences84000Config(
             name="registers_3_only_no_splits",
-            version=datasets.Version("0.1.2", ""),
+            version=datasets.Version("0.2.0", ""),
             description="Dataset for a Tibetan encoder with 3 registers, no augmentation, and all splits concatenated",
         ),
         ParallelSentences84000Config(
             name="registers_2_only",
-            version=datasets.Version("0.1.2", ""),
+            version=datasets.Version("0.2.0", ""),
             description="Dataset for a Tibetan encoder with 2 registers and no augmentation",
         ),
     ]
@@ -129,7 +129,7 @@ class ParallelSentences84000(datasets.GeneratorBasedBuilder):
                             os.path.join(files_path, "train.en"),
                             os.path.join(files_path, "valid.en"),
                             os.path.join(files_path, "test.en")
-                        ]
+                        ],
                     }
                 )
             ]
@@ -152,7 +152,7 @@ class ParallelSentences84000(datasets.GeneratorBasedBuilder):
                         "en_fn": os.path.join(files_path, "test.en")}),
             ]
 
-    def _generate_examples(self, bo_fn, en_fn):     # pylint: disable=arguments-differ
+    def _read_from_files(self, bo_fn, en_fn):
         if isinstance(bo_fn, list):
             if not isinstance(en_fn, list) or not len(bo_fn) == len(en_fn):
                 raise ValueError("Both input files must be either strings or lists of strings of the same length")
@@ -162,10 +162,7 @@ class ParallelSentences84000(datasets.GeneratorBasedBuilder):
                 with open(cur_bo_fn, encoding="utf-8") as bo_f, open(cur_en_fn, encoding="utf-8") as en_f:
                     for id_, (bo, en) in enumerate(zip(bo_f, en_f)):
                         id_ = cur_bo_fn + '|' + str(id_)
-                        yield id_, {
-                            "tibetan": bo.strip(),
-                            "english": en.strip()
-                        }
+                        yield id_, bo, en
             return
 
         if isinstance(en_fn, list):
@@ -176,7 +173,12 @@ class ParallelSentences84000(datasets.GeneratorBasedBuilder):
         with open(bo_fn, encoding="utf-8") as bo_f, open(en_fn, encoding="utf-8") as en_f:
             for id_, (bo, en) in enumerate(zip(bo_f, en_f)):
                 id_ = bo_fn + '|' + str(id_)
-                yield id_, {
-                    "tibetan": bo.strip(),
-                    "english": en.strip()
-                }
+                yield id_, bo, en
+
+    def _generate_examples(self, bo_fn, en_fn):     # pylint: disable=arguments-differ
+        for id_, bo, en in self._read_from_files(bo_fn, en_fn):
+            bo, en = bo.strip, en.strip()
+            yield id_, {
+                "tibetan": bo,
+                "english": en
+            }
