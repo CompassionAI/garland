8b4db065b9af665e821c34fa3922c39dc551e251
eisene <eeisenst@gmail.com>
2022-08-29 17:27:26 -0400

    Reset back to best model

--- cai_garland/models/pooled_context_decoder.py
+++ cai_garland/models/pooled_context_decoder.py
@@ -48,7 +48,7 @@ class BartDecoderWithPooledContext(BartDecoder):
     context into the token embedding slot for the starting token.
     """
 
-    context_architecture = ContextArchitecture.FrozenEmbeddingsWithTwoLayers
+    context_architecture = ContextArchitecture.DenseFeatureTransformer
 
     def __init__(self, config: BartConfig, embed_tokens: Optional[nn.Embedding] = None):
         super().__init__(config, embed_tokens=embed_tokens)
