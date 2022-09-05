fb0e9480e2e58bbb48e12a3e3f280faead3744f9
eisene <eeisenst@gmail.com>
2022-09-04 21:28:18 -0400

    Bugfix in CAI NLLB tokenizer decoder

--- cai_garland/models/cai_nllb_tokenizer.py
+++ cai_garland/models/cai_nllb_tokenizer.py
@@ -58,7 +58,7 @@ class CAINllbTokenizerFast(NllbTokenizerFast):
     def _decode(self, *args, **kwargs):
         if self.tokenizer_remapping_backward is not None:
             kwargs['token_ids'] = [self.tokenizer_remapping_backward[x] for x in kwargs['token_ids']]
-        return self._tokenizer._decode(*args, **kwargs)
+        return super()._decode(*args, **kwargs)
 
     @property
     def is_remapped(self):
