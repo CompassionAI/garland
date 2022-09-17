f95be2c36edcb4ca65fcadff6c2b90315abbc87a
eisene <eeisenst@gmail.com>
2022-09-17 19:17:17 -0400

    Bugfix in target special tokens

--- cai_garland/models/cai_nllb_tokenizer.py
+++ cai_garland/models/cai_nllb_tokenizer.py
@@ -51,8 +51,8 @@ class CAINllbTokenizerFast(NllbTokenizerFast):
         """Reset the special tokens to the target language setting. No suffix and prefix=[eos, tgt_lang_code].
             NB that this appears to be wrong in the HF NLLB tokenizer!"""
         self.cur_lang_code = self.convert_tokens_to_ids(lang)
-        self.prefix_tokens = [self.eos_token_id, self.cur_lang_code]
-        self.suffix_tokens = []
+        self.prefix_tokens = [self.cur_lang_code, self.cur_lang_code]
+        self.suffix_tokens = [self.eos_token_id]
 
         prefix_tokens_str = self.convert_ids_to_tokens(self.prefix_tokens)
         suffix_tokens_str = self.convert_ids_to_tokens(self.suffix_tokens)
