0eefe5b17d10134ad99ecd56dd9107676a694dc9
eisene <eeisenst@gmail.com>
2022-09-17 17:38:32 -0400

    Fix target language tokens in NLLB tokenizer

--- cai_garland/models/cai_nllb_tokenizer.py
+++ cai_garland/models/cai_nllb_tokenizer.py
@@ -2,6 +2,7 @@ import os
 import torch
 
 from transformers import NllbTokenizerFast
+from tokenizers import processors
 
 
 class CAINllbTokenizerFast(NllbTokenizerFast):
@@ -45,6 +46,22 @@ class CAINllbTokenizerFast(NllbTokenizerFast):
 
     def language_id(self, language_code):
         return self.tokenizer_remapping_forward[self.lang_code_to_id[language_code]]
+
+    def set_tgt_lang_special_tokens(self, lang: str) -> None:
+        """Reset the special tokens to the target language setting. No suffix and prefix=[eos, tgt_lang_code].
+            NB that this appears to be wrong in the HF NLLB tokenizer!"""
+        self.cur_lang_code = self.convert_tokens_to_ids(lang)
+        self.prefix_tokens = [self.eos_token_id, self.cur_lang_code]
+        self.suffix_tokens = []
+
+        prefix_tokens_str = self.convert_ids_to_tokens(self.prefix_tokens)
+        suffix_tokens_str = self.convert_ids_to_tokens(self.suffix_tokens)
+
+        self._tokenizer.post_processor = processors.TemplateProcessing(
+            single=prefix_tokens_str + ["$A"] + suffix_tokens_str,
+            pair=prefix_tokens_str + ["$A", "$B"] + suffix_tokens_str,
+            special_tokens=list(zip(prefix_tokens_str + suffix_tokens_str, self.prefix_tokens + self.suffix_tokens)),
+        )
 
     def _fix_target_tokens(self, input_):
         if getattr(input_, "tokens_fixed", False):
